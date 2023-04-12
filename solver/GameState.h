d97b5fc69837b150023aba7460599d0b0ed62d15
Flynn Owen <flynnoo49@gmail.com>
2023-04-12 21:37:35 +1200

    add wins / draws / losses to GameState

--- solver/GameState.h
+++ solver/GameState.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <stack>
 #include "Board.h"
 
 using namespace std;
@@ -13,22 +14,32 @@ class GameState{
     // Snapshot of a game a particular time.
     public:
         int player_turn;
-        int wins {0};
-        int losses {0};
-        int draws {0};
-        bool solved;
+        // count of wins, draws and losses per possible column.
+        map<int, int> wins {{0, 0}, 
+                            {1 ,0},
+                            {2, 0},
+                            {3 ,0},
+                            {4, 0},
+                            {5, 0},
+                            {6 ,0}};
+        map<int, int> draws {{0, 0}, 
+                             {1 ,0},
+                             {2, 0},
+                             {3 ,0},
+                             {4, 0},
+                             {5, 0},
+                             {6 ,0}};
+        map<int, int> losses {{0, 0}, 
+                              {1 ,0},
+                              {2, 0},
+                              {3 ,0},
+                              {4, 0},
+                              {5, 0},
+                              {6 ,0}};
+        vector<int> to_try {0, 1, 2, 3, 4, 5, 6};
 
-    GameState(int player_turn_val, 
-              int wins_val, 
-              int losses_val, 
-              int draws_val, 
-              bool solved_val)
-    : player_turn{player_turn_val},
-    wins{wins_val},
-    losses{losses_val},
-    draws{draws_val},
-    solved{solved_val}{};
-
+    GameState(int player_turn_val)
+    : player_turn{player_turn_val}{};
 };
 
 
